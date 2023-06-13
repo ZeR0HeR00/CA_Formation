ac0ee2107c5d32b87fd5f9a96957f225ede438ef
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-13 17:29:17 +0200

    OK

--- Terre/terre00.py
+++ Terre/terre00.py
@@ -1,23 +1,21 @@
-############### Eau : ok ###############
+############### Terre : ok ###############
 
 ### Function ###
 
+def alphabet():
 
+    a = 97
+    z = 123
 
+    for i in range(a, z):
+        print(chr(i), end = "")
 
 ### Error ###
-
-
-
-
 ### Parsing ###
-
-
-
-
 ### Problem solving ###
 
-
-
+resultat = alphabet()
 
 ### Result ###
+
+print(resultat)
