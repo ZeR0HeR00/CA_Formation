2870298d8f50faf12c9443e61d9005784dbd1f39
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-14 12:37:35 +0200

    OK

--- Terre/terre02.py
+++ Terre/terre02.py
@@ -1,23 +1,35 @@
-############### Eau : ok ###############
+############### Terre : ok ###############
+""" 
+
+Afficheur d'argument
+
+Créer un programme qui affiche les arguments qu'il reçoit ligne par ligne,
+peu importe le nombre d'arguments.
+
+"""
+
+import sys
 
 ### Function ###
 
+def get_argument(user_term):
+
+    for argument in user_term:
+        print(argument)
+    exit()
 
 
 
 ### Error ###
 
-
-
-
 ### Parsing ###
 
-
-
+user = sys.argv[1:]
 
 ### Problem solving ###
 
-
-
+resultat = get_argument(user)
 
 ### Result ###
+
+print(resultat)
--- Terre/terre03.py
+++ Terre/terre03.py
@@ -1,23 +1,36 @@
-############### Eau : ok ###############
+############### Terre : ok ###############
+"""
+
+L'alphabet à partir de
+
+Créez un programme qui affiche l'alphabet à partir de la lettre donnée en argument en 
+lettres minuscules suivi d'un retour à la ligne.
+
+"""
+
+import sys
 
 ### Function ###
 
+def alphabet():
 
+    a = 97
+    z = 123
 
+    for i in range(a, z):
+        print(chr(i), end = "")
 
+def find_
 ### Error ###
-
-
-
 
 ### Parsing ###
 
-
-
+letter = sys.argv
 
 ### Problem solving ###
 
-
-
+resultat = alphabet()
 
 ### Result ###
+
+print(resultat)
