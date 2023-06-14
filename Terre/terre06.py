3ac86ffd75d9418559e138210b293677705ccad8
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-14 18:35:09 +0200

    OK

--- Terre/terre06.py
+++ Terre/terre06.py
@@ -1,23 +1,39 @@
-############### Eau : ok ###############
+############### Terre : ok ###############
+"""
+
+Inverser une chaîne
+
+Créez un programme qui affiche l'inverse de la chaîne de caractères donnée en argument.
+
+"""
+
+import sys
 
 ### Function ###
 
+def reverse_word(word):
 
+    return word[::-1]
 
+def error():
+    
+    if len(sys.argv[1]) == 0:
+        print("erreur.")
+        exit()
 
 ### Error ###
 
-
-
+error()
 
 ### Parsing ###
 
-
+word = sys.argv[1:]
 
 
 ### Problem solving ###
 
-
-
+resultat_reverse = " ".join(reverse_word(word))
 
 ### Result ###
+
+print(resultat_reverse)
