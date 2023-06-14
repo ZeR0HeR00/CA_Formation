6ee204e85196becbf50f8b9806a8be453addfdf3
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-15 01:17:52 +0200

    OK

--- Terre/terre09.py
+++ Terre/terre09.py
@@ -1,23 +1,34 @@
-############### Eau : ok ###############
+############### Terre : ok ###############
+
+import sys
 
 ### Function ###
 
+def square_root(base):
+
+    resultat = pow(base, 0.5)
+    return resultat
 
 
+def error():
+    
+    if int(sys.argv[1]) <= 0:
+        print("erreur")
+        exit()
 
 ### Error ###
 
-
-
+error()
 
 ### Parsing ###
 
-
-
+base = int(sys.argv[1])
 
 ### Problem solving ###
 
-
+resultat_square = square_root(base)
 
 
 ### Result ###
+
+print(f"Le résultat est : {resultat_square}")
