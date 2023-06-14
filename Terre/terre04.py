e0d2c2fb25a9eb8f5d6572b0ec76d1f80aa56ba2
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-14 16:03:45 +0200

    OK

--- Terre/terre04.py
+++ Terre/terre04.py
@@ -1,23 +1,50 @@
 ############### Eau : ok ###############
+"""
+
+Pair ou impair
+
+Créez un programme qui permet de déterminer si l'argument donné est un entier pair ou impair.
+
+"""
+
+import sys
 
 ### Function ###
 
+def even_or_odd(number):
 
+    val = int(number)
+
+    if val % 2 == 0:
+        return "pair"
+    else:
+        return "impair"
+
+
+def error():
+
+    if len(sys.argv) != 2:
+        print("erreur.")
+        exit()
+    
+    if not sys.argv[1].isdigit():
+        print("erreur")
+        exit()
 
 
 ### Error ###
 
-
-
+error()
 
 ### Parsing ###
 
-
-
+number = sys.argv[1]
 
 ### Problem solving ###
 
-
+resultat = even_or_odd(number)
 
 
 ### Result ###
+
+print(resultat)
