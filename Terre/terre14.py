97a98d401734feeb4d5b3d5183992b2c83c1d6a7
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-15 15:23:52 +0200

    OK

--- Terre/terre14.py
+++ Terre/terre14.py
@@ -1,8 +1,24 @@
 ############### Eau : ok ###############
+"""
+
+Trié ou pas
+
+Créez un programme qui détermine si une liste d'entiers est triée ou pas.
+
+"""
+
+import sys
 
 ### Function ###
 
+def sorted_not_sorted(first_number, second_number, third_number):
 
+    if int(first_number) < int(second_number) and int(second_number) < int(third_number):
+        print("Triée !")
+        exit()
+    else:
+        print("Pas triée !")
+        exit()
 
 
 ### Error ###
@@ -12,12 +28,14 @@
 
 ### Parsing ###
 
-
-
+first_number = sys.argv[1]
+seconde_number = sys.argv[2]
+third_number= sys.argv[3]
 
 ### Problem solving ###
 
-
-
+resultat = sorted_not_sorted(first_number, seconde_number, third_number)
 
 ### Result ###
+
+print(resultat)
