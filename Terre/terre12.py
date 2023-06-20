8168638b7b5436cbe373efe0e0153a93eec4f2e1
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-20 17:08:53 +0200

    OK

--- Terre/terre12.py
+++ Terre/terre12.py
@@ -16,24 +16,42 @@ def time_conversion(hour_time,am_or_pm):
     hour, minute = int("".join(hour_and_minute[0])), int(
         "".join(hour_and_minute[1]))
     
-    if hour >= 12 and am_or_pm == "AM":
-        hour -= 12
-        return f"{hour}:{minute}"
-    elif am_or_pm == "PM" and hour >= 1 or hour <= 11:
-        hour += 12
-        return f"{hour}:{minute}"
-    elif am_or_pm == "AM" and hour >= 1 or hour <= 12:
-        return f"{hour}:{minute}"
-   
-    
+    if am_or_pm == "AM":
+
+        if hour >= 12:
+            return f"{hour - 12}:{minute}"
+        
+        if hour >= 1 or hour <= 12:
+            return f"{hour}:{minute}"
+        
+    if am_or_pm == "PM":
+
+        if hour >= 1 or hour <= 11:
+            return f"{hour + 12}:{minute}"
+
+def handle_error():
+
+    hour_and_minute = time_conversion_split(sys.argv[1])
+
+    if not hour_and_minute[0].isdigit() or not hour_and_minute[1].isdigit():
+        print("Heure et minute doivent correspondre à des nombre")
+        exit()
+    elif len(hour_and_minute) != 2 or len(hour_and_minute[0]) > 2 or len(hour_and_minute[1]) > 2:
+        print("Veuillez à entrer le bon modèle d'heure et de minutes")
+        exit()
+    elif int(hour_and_minute[0]) > 12 or int(hour_and_minute[1]) > 59:
+        print("Les heures ne peuvent dépasser 12 et les minutes 59.")
+        exit()
+
+
 ### Error ###
 
-
+handle_error()
 
 ### Parsing ###
 
 hour_complet = sys.argv[1]
-am_pm = sys.argv[2]
+am_pm = sys.argv[2].upper
 ### Problem solving ###
 
 resultat = time_conversion(hour_complet,am_pm)
