1a14e150d10a069dd6b2a565ac1e66604878549c
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-20 11:35:22 +0200

    Add new error and naming fix

--- Terre/terre11.py
+++ Terre/terre11.py
@@ -4,17 +4,20 @@ import sys
 
 ### Function ###
 
+
 def time_conversion_split(hour_time):
 
     hour_time = "".join(hour_time).split(":")
     return hour_time
 
+
 def time_conversion(hour_time, am, pm):
 
     hour_and_minute = time_conversion_split(hour_time)
 
-    hour, minute = int("".join(hour_and_minute[0])), int("".join(hour_and_minute[1]))
-    
+    hour, minute = int("".join(hour_and_minute[0])), int(
+        "".join(hour_and_minute[1]))
+
     if hour == 00:
         hour = 12
         return f"{hour}:{minute} {am}"
@@ -28,15 +31,19 @@ def time_conversion(hour_time, am, pm):
     else:
         return f"{hour}:{minute} {pm}"
 
+
 def error():
 
     hour_and_minute = time_conversion_split(sys.argv[1:])
 
     if not hour_and_minute[0].isdigit() or not hour_and_minute[1].isdigit():
-        print("heure et minute doivent correspandre à des nombre")
+        print("heure et minute doivent correspondre à des nombre")
         exit()
     elif len(hour_and_minute) != 2 or len(hour_and_minute[0]) > 2 or len(hour_and_minute[1]) > 2:
-        print("erreur")
+        print("Veuillez à entrer le bon modèle d'heure et de minutes")
+        exit()
+    elif int(hour_and_minute[0]) > 23 or int(hour_and_minute[1]) > 59:
+        print("les heures ne peuvent dépasser 23 et les minutes 59.")
         exit()
 
 
