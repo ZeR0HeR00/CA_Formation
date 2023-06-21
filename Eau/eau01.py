f96fd4dede9ec95e3e21aba15e7b5fed7b5ef803
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-21 13:07:31 +0200

    Exercice OK

--- Eau/eau01.py
+++ Eau/eau01.py
@@ -1,8 +1,17 @@
 ############### Eau : ok ###############
 
 ### Function ###
+def combinaison():
 
+    for first_number in range(0, 99):
+        first_number = str(first_number).zfill(2)
+        
+        for second_number in range(0, 99):
+            second_number = str(second_number).zfill(2)
 
+            if first_number < second_number:
+                print(f"{first_number} {second_number}")
+    exit()
 
 
 ### Error ###
@@ -17,7 +26,8 @@
 
 ### Problem solving ###
 
-
-
+resultat = combinaison()
 
 ### Result ###
+
+print(resultat)
