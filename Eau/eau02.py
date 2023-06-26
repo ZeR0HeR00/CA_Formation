6427a0e0b634f776f36f7e87210bb75f31d369b6
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-26 13:37:40 +0200

    OK

--- Eau/eau02.py
+++ Eau/eau02.py
@@ -1,23 +1,33 @@
 ############### Eau : ok ###############
 
+import sys 
+
 ### Function ###
 
+def reverse(argument):
 
+    get_argument = argument
+    print("\n".join(get_argument[::-1]))
+    exit()
 
+def handle_error():
+
+    if len(sys.argv) < 2:
+        print("Il n'y a aucun arguemnt")
+        exit()
 
 ### Error ###
 
-
-
+handle_error()
 
 ### Parsing ###
 
-
-
+argument = sys.argv[1:]
 
 ### Problem solving ###
 
-
-
+resultat = reverse(argument)
 
 ### Result ###
+
+print(resultat)
