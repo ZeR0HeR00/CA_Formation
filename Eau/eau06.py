f828f0ee28dd6d37dcb1b36579acfb1b3c110717
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-27 20:21:44 +0200

    Using loop and condition to resolve uppercase/2

--- Eau/eau06.py
+++ Eau/eau06.py
@@ -1,23 +1,46 @@
 ############### Eau : ok ###############
-
+import sys
 ### Function ###
 
+def uppercase_over_two(text):
+    
+    text = text.lower()
+
+    result = ""
+
+    for i, char in enumerate(text):
+        
+        if i % 2 == 0:
+            result += char.upper()
+        else:
+            result += char
+
+    return result
+    
 
 
+def handle_error():
+
+    if len(sys.argv) != 2:
+        print("Error")
+        exit()
+        
+    if sys.argv[1].isdigit():
+        print("Please enter a string")
+        exit()
 
 ### Error ###
 
-
-
+handle_error()
 
 ### Parsing ###
 
-
-
+text = str(sys.argv[1])
 
 ### Problem solving ###
 
-
-
+resultat = uppercase_over_two(text)
 
 ### Result ###
+
+print(resultat)
