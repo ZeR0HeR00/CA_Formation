e1f7c8bced4c313f75fccc15579d88fd160c725a
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-27 17:57:24 +0200

    variable renaming

--- Eau/eau05.py
+++ Eau/eau05.py
@@ -4,9 +4,9 @@ import sys
 
 def comparaison(chaine_one, chaine_two):
     
-    for debut in range(0, len(chaine_one) - len(chaine_two) + 1):
+    for beginning in range(0, len(chaine_one) - len(chaine_two) + 1):
         
-        if chaine_two == chaine_one[debut : debut + len(chaine_two)]:
+        if chaine_two == chaine_one[beginning : beginning + len(chaine_two)]:
             return True
 
     return False
