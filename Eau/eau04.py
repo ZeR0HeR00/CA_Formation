8d58c28960b84e453993ef63af8476f5d27ab652
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-06-27 13:23:17 +0200

    Using recursion to solve the next prime number

--- Eau/eau04.py
+++ Eau/eau04.py
@@ -1,23 +1,41 @@
 ############### Eau : ok ###############
 
+import sys
 ### Function ###
 
+def next_prime_number(number):
+    
+    i = 2
+    while i < number and number % i != 0:
+        i += 1
+    
+    if i == number:
+        return f"The next prime number is : {number}"
+    else:
+        return f"{next_prime_number(number + 1)}"
 
+def handle_error():
 
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Please enter a number")
+        exit()
+    
+    if  int(sys.argv[1]) < 0:
+        print("-1")
+        exit()
 
 ### Error ###
 
-
-
+handle_error()
 
 ### Parsing ###
 
-
-
+prime_number = int(sys.argv[1])
 
 ### Problem solving ###
 
-
-
+resultat = next_prime_number(prime_number)
 
 ### Result ###
+
+print(resultat)
