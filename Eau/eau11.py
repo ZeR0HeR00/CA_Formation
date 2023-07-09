260eb2b4e0e9c5d0a7842d52621dcf07003d7006
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-07-09 22:44:19 +0200

    Absolut minimum difference finish

--- Eau/eau11.py
+++ Eau/eau11.py
@@ -1,23 +1,47 @@
 ############### Eau : ok ###############
 
+import sys
+
 ### Function ###
+    
+def absolut_min_difference(number_array):
+    try:
 
+        for i in range(len(number_array)):
+            number_array[i] = int(number_array[i])
 
+    
+    except ValueError:
+        print("Enter numbers only")
+        exit()
 
+    number_array = sorted(number_array)
+   
+    minimum = abs(number_array[0] - number_array[1])
+
+    for i in range(1, len(number_array) - 1):
+        result_soustraction = abs(number_array[i] - number_array[i + 1])
+            
+        if minimum > result_soustraction:
+            minimum = result_soustraction
+    
+    return minimum
+    
+def handle_error():
+
+    if len(sys.argv) == 2:
+        print("Error")
+        exit()
 
 ### Error ###
 
-
+handle_error()
 
 
 ### Parsing ###
 
+number_array = sys.argv[1:]
 
+### Problem solving & result###
 
-
-### Problem solving ###
-
-
-
-
-### Result ###
+print(absolut_min_difference(number_array))
