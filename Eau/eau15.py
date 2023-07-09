dc3bf9b289f6c33e94222ee719e420122fcef14f
ZeR0HeR00 <Zer0_AbS0luT@proton.me>
2023-07-09 23:31:14 +0200

    Eau finish

--- Eau/eau15.py
+++ Eau/eau15.py
@@ -1,23 +1,5 @@
 ############### Eau : ok ###############
 
-### Function ###
+eau_ok = "plus intérrésant que la terre, j'ai hâte de manger du array, d'ailleurs je ne suis pas sortis trempé  :')"
 
-
-
-
-### Error ###
-
-
-
-
-### Parsing ###
-
-
-
-
-### Problem solving ###
-
-
-
-
-### Result ###
+print(f"J'ai terminé l'épreuve de l'Eau et c'était {eau_ok}")
